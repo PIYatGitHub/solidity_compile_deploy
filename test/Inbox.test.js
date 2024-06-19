d6342cd68c71a95fc8dedd3802fdcde9f73e17f3
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-06-19 22:49:47 +0300

    run some sample tests -- check if mocha setup works

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,11 @@
 const ganache = require('ganache');
-const { Web3 } = require('web3');
+const { Web3 } = require('web3'); // pay attention to capital and lowercase letters!
+const assert = require("assert"); 
 
-// updated ganache and web3 imports added for convenience
+//make a client connecting to the local eth network
+const web3 = new Web3(ganache.provider());
 
-// contract test code will go here
+
+it('can run', ()=>{
+    assert.equal(2, 2)
+})
