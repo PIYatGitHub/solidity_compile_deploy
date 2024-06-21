a84010004a6e8645957aa4fdd3f421119dd31b67
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-06-21 23:34:52 +0300

    make the first contract deploy

--- deploy.js
+++ deploy.js
@@ -1,5 +1,31 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const { Web3 } = require('web3')
+const compiled = require("./compile")
+require('dotenv').config()
 
-// deploy code will go here
+const INITIAL_STRING = 'A distant Roman city'
+
+const provider = new HDWalletProvider(
+    process.env.MNEUMONIC,
+    'https://sepolia.infura.io/v3/' + process.env.INFURA_API_KEY
+)
+
+
+const web3 = new Web3(provider)
+
+async function deploy() {
+    const accounts = await web3.eth.getAccounts()
+    const selectedAccount = accounts.at(0)
+
+    console.log("Attempting to deploy with account: ", selectedAccount)
+
+    //deploy the contract to one of them 
+    const resultInstance = await new web3.eth.Contract(compiled.abi)
+        .deploy({ data: compiled.evm.bytecode.object, arguments: [INITIAL_STRING] })
+        .send({ from: selectedAccount, gas: '1000000' })
+
+    console.log("This contract just got deployed to this address!", resultInstance.options.address)
+}
+
+// we made this so we can use the async/await! 
+deploy() 
